549c47599c65e863ef36c04c75a4d09dcf38d573
JiHo Han <sciencejiho@gmail.com>
2020-01-10 02:24:52 +0900

    Add the last part of chap5-1.cpp manually
    This commit will manually add the last part of the lyrics required, thus completing the chap5-1.cpp. Compiled version of chap5-1.cpp is also included.

--- Chapter5/chap5-1.cpp
+++ Chapter5/chap5-1.cpp
@@ -6,11 +6,18 @@ int main()
 {
     int i;
     
-    for (i = 99; i > 1; i-- )
+    for (i = 99; i > 2; i-- )
     {
         cout << i << " bottles of beer on the wall, " << i << " bottles of beer." << "\n";
-        cout << "Take one down and pass it around, " << i-1 << "  bottles of beer on the wall." << "\n";
+        cout << "Take one down and pass it around, " << i-1 << "bottles of beer on the wall." << "\n";
     }
+
+    cout << "2 bottles of beer on the wall, 2 bottles of beer." << "\n";
+    cout << "Take one down and pass it around, 1 bottle of beer on the wall." << "\n";
+    cout << "1 bottle of beer on the wall, 1 bottle of beer." << "\n";
+    cout << "Take one down and pass it around, no more bottles of beer on the wall." << "\n";
+    cout << "No more bottles of beer on the wall, no more bottles of beer." << "\n";
+    cout << "Go to the store and buy some more, 99 bottles of beer on the wall." << "\n";
 
     return 0;
 }
