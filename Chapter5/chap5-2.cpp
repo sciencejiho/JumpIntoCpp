3e3df7dc253ab3a6e08d2646bd529df89ba60eba
JiHo Han <sciencejiho@gmail.com>
2020-01-10 18:06:12 +0900

    Add do-while loop for chap5-2.cpp
    chap5-2.cpp is a program that prompts the user to choose an option out of selections. If the selection is invalid, it will prompt again for a valid input. The output when correct option is chosen is not updated.

---
+++ Chapter5/chap5-2.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+
+ using namespace std;
+ 
+ int main()
+ {
+     int input;
+     
+     do
+     {
+         cout << "Please input a selection. (1, 2, 3, and 4): ";
+         cin >> input;
+     } while (input != 1 || input != 2 || input != 3 || input != 4)
+      
+ }
+
