c8fb640d3bd4748e06a371c55abe071a4a48bac5
JiHo Han <sciencejiho@gmail.com>
2020-01-10 18:42:22 +0900

    Fix a bug where chap5-2.cpp didn't work as intended
    This commit changes the OR operator into AND operator so that if the input does not match to any of the correct choices, the question will be prompted again. chap5-2, which is the compiled version of chap5-2.cpp, is also added.

--- Chapter5/chap5-2.cpp
+++ Chapter5/chap5-2.cpp
@@ -10,7 +10,7 @@
      {
          cout << "Please input a selection. (1, 2, 3, and 4): ";
          cin >> input;
-     } while (input != 1 || input != 2 || input != 3 || input != 4)
+     } while (input != 1 && input != 2 && input != 3 && input != 4);
       
  }
 
